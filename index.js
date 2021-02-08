38c8ab5b9354b2d2c6f08f5f473e17fe4b76bfb8
ferdy <xv.ferdy@gmail.com>
2021-02-08 18:51:44 +0700

    Coba tampilkan HTML

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hi there");
+  res.send(`
+    <div>
+    <form>
+      <input placeholder="email" />
+      <input placeholder="password" />
+      <input placeholder="password confirmation" />
+      <button>Sign Up</button>
+    </form>
+  </div>
+    `);
 });
 
 app.listen(3000, () => {
