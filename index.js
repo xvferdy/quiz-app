54561f0f8f95d1a3e319736ee25befe0d3d5207e
ferdy <xv.ferdy@gmail.com>
2021-02-09 18:33:03 +0700

    Tes endpoint

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => {
-  res.send(`
-    <div>
-    <form>
-      <input placeholder="email" />
-      <input placeholder="password" />
-      <input placeholder="password confirmation" />
-      <button>Sign Up</button>
-    </form>
-  </div>
-    `);
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// app.get("/", (req, res) => {
+//   res.send(`
+//     <div>
+//     <form action="/" method="POST">
+//       <input placeholder="email" name="email!"/>
+//       <input placeholder="password" />
+//       <input placeholder="password confirmation" />
+//       <button>Sign Up</button>
+//     </form>
+//   </div>
+//     `);
+// });
+
+// app.post("/", (req, res) => {
+//   console.log(req.body);
+// });
+
+app.get("/users", (req, res) => {
+  res.json(userList);
 });
 
 app.listen(3000, () => {
@@ -19,3 +34,11 @@ app.listen(3000, () => {
 });
 
 console.log("inside console");
+
+//====================
+
+let userList = [
+  { name: "ferdy", age: 24, married: false },
+  { name: "clara", age: 19, married: false },
+  { name: "beni", age: 20, married: true },
+];
